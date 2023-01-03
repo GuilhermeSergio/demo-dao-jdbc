f06c5004f6cee3a03d5faaad31aaf824f65e6b8d
GuilhermeSergio <gui.sergiiio@gmail.com>
2023-01-02 21:04:34 -0300

    novo commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -22,6 +23,14 @@ public class Program {
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
 		
+		System.out.println("\n=== TEST 3: Seller findAll ===");
+		list = sellerDao.findAll();
+		list.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 4: Seller findAll ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.print("Numero do ID inserido: " + newSeller.getId());
 	}
 
 }
